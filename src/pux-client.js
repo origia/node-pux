a7e2122636e460cebf574d678dd7371b1ffa7496
Daniel Perez <tuvistavie@gmail.com>
2014-03-09 11:42:03 +0900

    Add extract candidate functionality. Read API_KEY from env.

--- src/pux-client.js
+++ src/pux-client.js
@@ -4,8 +4,9 @@
 
 "use strict";
 
-var _       = require('underscore')
-  , request = require('request')
+var _           = require('underscore')
+  , request     = require('request')
+  , parseString = require('xml2js').parseString
 
 
 var defaults = {
@@ -19,14 +20,37 @@ var PuxClient = function (apiKey, overrides) {
 
 _.extend(PuxClient.prototype, {
 
-  authenticateFaceBase64: function (b64Image) {
+  authenticateFaceBase64: function (b64Image, options) {
+    options = options || {}
+    var self = this
     var data = { apiKey:      this.apiKey
                , mode:        'verify'
                , inputBase64: b64Image
                }
-    request.post(this.options.endPoint, { form: data }, function (err, msg, response) {
-      console.log(response)
+    request.post(this.options.endPoint, { form: data }, function (err, response, body) {
+      parseString(body, function (err, result) {
+        if (err === null) {
+          if (response.statusCode === 200) {
+            var resultData = result.results.faceRecognition[0]
+            if (options.extractCandidate)
+              resultData = self.extractCandidate(resultData)
+            if (options.onSuccess) options.onSuccess(resultData)
+          } else {
+            if (options.onError) options.onError(result)
+          }
+        } else {
+          if (options.onError) options.onError(err)
+        }
+      })
     })
+  }
+
+, extractCandidate: function (result) {
+    if (!result.verificationFaceInfo[0].candidate) return null
+    var firstCandidate = result.verificationFaceInfo[0].candidate[0]
+      , score = firstCandidate.score[0]
+    if (score > 50) return firstCandidate.faceId[0]
+    else return null
   }
 })
 
